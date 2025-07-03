31e6140a752448751d6fc40989d7d8bf9d0ac789
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2025-07-03 13:27:34 -0300

    - criando get personalidades.go

--- routes/routes.go
+++ routes/routes.go
@@ -10,8 +10,8 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personas", controllers.AllPersonas)
-	r.HandleFunc("/api/personas/{id}", controllers.GetPersona)
+	r.HandleFunc("/api/personas", controllers.AllPersonas).Methods("GET")
+	r.HandleFunc("/api/personas/{id}", controllers.GetPersona).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
