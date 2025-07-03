88d93c023b144056f4a427cc5b5a820377a06dec
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2025-07-03 17:40:59 -0300

    finalização

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/NicolasKonishi/go-rest-api/database"
-	"github.com/NicolasKonishi/go-rest-api/models"
 	"github.com/NicolasKonishi/go-rest-api/routes"
 )
 
 func main() {
-	models.Personas = []models.Personalidades{
-		{Id: 1, Nome: "nome1", Historia: "historia1"},
-		{Id: 2, Nome: "nome2", Historia: "historia2"},
-	}
-
+	//models.Personas = []models.Personalidades{
+	//	{Id: 1, Nome: "nome1", Historia: "historia1"},
+	//	{Id: 2, Nome: "nome2", Historia: "historia2"},
+	//} -> começo de quando não tinha bd
 	database.InitDB()
 
-	fmt.Println("iniciando server...http://localhost:8080/")
+	fmt.Println("iniciando server...http://localhost:8080/api/personas")
 	routes.HandleRequest()
 }
