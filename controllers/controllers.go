1bacb50f057899b179dd7c9667e9c3b8288bda6d
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2025-07-03 14:47:58 -0300

    - criando metodos de deletar e editar

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,7 +14,7 @@ func Home(w http.ResponseWriter, req *http.Request) {
 }
 func AllPersonas(w http.ResponseWriter, req *http.Request) {
 	var p []models.Personalidades
-	//DB.Find(&p) -> encontra no endereço de memória a var p
+	//DB.Find(&p) -> encontra no endereço de memória a var ps
 	database.DB.Find(&p)
 	json.NewEncoder(w).Encode(p)
 }
@@ -25,4 +25,34 @@ func GetPersona(w http.ResponseWriter, req *http.Request) {
 	//First(&p, id) -> retorna o primeiro que encontra de acordo com o id
 	database.DB.First(&p, id)
 	json.NewEncoder(w).Encode(p)
+}
+
+func CreatePersona(w http.ResponseWriter, req *http.Request) {
+	var p models.Personalidades
+	//NewDecoder -> utiliza decoder ao inves de encoder para transformar json em linguagem que o go entende para salvar no banco
+	json.NewDecoder(req.Body).Decode(&p)
+	database.DB.Create(&p)
+	// retorna a personalidade que foi criada
+	json.NewEncoder(w).Encode(p)
+}
+
+func DeletePersona(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	id := vars["id"]
+	var p models.Personalidades
+
+	database.DB.Delete(&p, id)
+	json.NewEncoder(w).Encode(p)
+}
+
+func EditPersona(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	id := vars["id"]
+	var p models.Personalidades
+	database.DB.First(&p, id)
+
+	json.NewDecoder(req.Body).Decode(&p)
+	database.DB.Save(&p)
+	json.NewEncoder(w).Encode(p)
+
 }
--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,9 @@ func HandleRequest() {
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personas", controllers.AllPersonas).Methods("GET")
 	r.HandleFunc("/api/personas/{id}", controllers.GetPersona).Methods("GET")
+	r.HandleFunc("/api/personas", controllers.CreatePersona).Methods("POST")
+	r.HandleFunc("/api/personas/{id}", controllers.DeletePersona).Methods("DELETE")
+	r.HandleFunc("/api/personas/", controllers.EditPersona).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
